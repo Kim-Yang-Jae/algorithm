d70e7645d55cad0c54113a2874faf09963112597
김양재 <kyj95112@naver.com>
2022-07-28 22:44:40 +0900

    q1330 q9498 q2753 q14681 q2884

---
+++ src/com/baekjoon/q2753/Main.java
@@ -0,0 +1,27 @@
+package com.baekjoon.q2753;
+
+import java.util.Scanner;
+
+/**
+ *  윤년 계산
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+        int isLeapYear = 0;
+
+        if (year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0)
+                    isLeapYear = 1;
+            }else
+                isLeapYear = 1;
+        }
+
+        System.out.println(isLeapYear);
+        sc.close();
+    }
+}
